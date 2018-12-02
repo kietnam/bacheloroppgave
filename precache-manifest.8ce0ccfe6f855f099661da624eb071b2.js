531b3e0f3ce5bdc8b508913ed140d15080870a32
kietnam <andre-ng@live.no>
2018-12-02 12:44:28 +0100

    Updates

--- precache-manifest.8ce0ccfe6f855f099661da624eb071b2.js
+++ precache-manifest.8ce0ccfe6f855f099661da624eb071b2.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "94981535f13ebdff977d",
-    "url": "/static/js/main.94981535.chunk.js"
+    "revision": "7391417cacd341a8585c",
+    "url": "/static/js/main.7391417c.chunk.js"
   },
   {
     "revision": "308598debfd8adf98ce6",
     "url": "/static/js/1.308598de.chunk.js"
   },
   {
-    "revision": "d0c743f4edd956fb5edb29aae48e8a70",
+    "revision": "511df3fda6e62f0ee53955f88c02042a",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a9a843022ff75ddc805ce8206fd35bf.js"
+  "/precache-manifest.8ce0ccfe6f855f099661da624eb071b2.js"
 );
 
 workbox.clientsClaim();
