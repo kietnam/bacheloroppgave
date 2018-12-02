7eca7c20d7605dfa626dc869fc022b70fe847e1b
kietnam <andre-ng@live.no>
2018-12-02 12:22:33 +0100

    Updates

---
+++ precache-manifest.2362e89c8772e8d52d8db82aa62ba33e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "1535927b43c4cd7e0712",
+    "url": "/react/static/js/main.1535927b.chunk.js"
+  },
+  {
+    "revision": "308598debfd8adf98ce6",
+    "url": "/react/static/js/1.308598de.chunk.js"
+  },
+  {
+    "revision": "144e3ff86e1e4559f3a8ce60336bfc26",
+    "url": "/react/index.html"
+  }
+];
